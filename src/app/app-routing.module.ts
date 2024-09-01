67b60b0bab59de7600b986c75cf149a697b731bd
Alessandro Casale <webmastercasale@gmail.com>
2024-09-01 09:56:25 +0200

    Lavoro della domenica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,37 +1,60 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LayoutTabsPage } from './views/layout-tabs/layout-tabs.page';
 
 const routes: Routes = [
   {
-    path: 'myoutfit',
-    loadChildren: () => import('./views/myoutfit/myoutfit.module').then( m => m.MyOutFitPageModule)
-  },
-  
-  {
-    path: 'register',
-    loadChildren: () => import('./views/register/register.module').then( m => m.RegisterPageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./views/login/login.module').then( m => m.LoginPageModule)
-  },
-  
-  {
-    path: 'add-outfit',
-    loadChildren: () => import('./views/add-outfit/add-outfit.module').then( m => m.AddOutfitPageModule)
-  },
-  {
-    path: 'my-wardrobes',
-    loadChildren: () => import('./views/my-wardrobes/my-wardrobes.module').then( m => m.MyWardrobesPageModule)
-  },  {
-    path: 'my-profile',
-    loadChildren: () => import('./views/my-profile/my-profile.module').then( m => m.MyProfilePageModule)
-  },
+    path: 'tabs',
+    component: LayoutTabsPage,
+   
+    children: [
 
 
-  
 
+      {
+        path: 'myoutfit',
+        loadChildren: () => import('./views/myoutfit/myoutfit.module').then(m => m.MyOutFitPageModule)
+      },
 
+      {
+        path: 'register',
+        loadChildren: () => import('./views/register/register.module').then(m => m.RegisterPageModule)
+      },
+      {
+        path: 'login',
+        loadChildren: () => import('./views/login/login.module').then(m => m.LoginPageModule)
+      },
+
+      {
+        path: 'add-outfit',
+        loadChildren: () => import('./views/add-outfit/add-outfit.module').then(m => m.AddOutfitPageModule)
+      },
+      {
+        path: 'my-wardrobes',
+        loadChildren: () => import('./views/my-wardrobes/my-wardrobes.module').then(m => m.MyWardrobesPageModule)
+      },
+      {
+        path: 'my-profile',
+        loadChildren: () => import('./views/my-profile/my-profile.module').then(m => m.MyProfilePageModule)
+      },
+      {
+        path: 'layout-tabs',
+        loadChildren: () => import('./views/layout-tabs/layout-tabs.module').then(m => m.LayoutTabsPageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/home',
+        pathMatch: 'full'
+      }
+    ]
+
+  },
+
+  {
+    path: '',
+    redirectTo: 'tabs/myoutfit',
+    pathMatch: 'full'
+  }
 
 ];
 
--- src/app/service/interface/user-interface.ts
+++ src/app/service/interface/user-interface.ts
@@ -17,3 +17,4 @@ export interface UserPreference {
   brend?: string[]
   style?: string[],
 }
+
