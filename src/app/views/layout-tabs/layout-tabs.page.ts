72c72a67e9fa74df3bb90cab83b3e5fc7c3ab745
webmastercasale <webmastercasale@gmail.com>
2024-09-17 12:05:43 +0200

    layout

--- src/app/views/layout-tabs/layout-tabs.page.ts
+++ src/app/views/layout-tabs/layout-tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-layout-tabs',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutTabsPage implements OnInit {
 
-  constructor() { }
+  constructor(private modalController: ModalController,private navController: NavController) { }
 
   ngOnInit() {
   }
-
+  async handleBackButton() {
+    // Controlla se la pagina è aperta in un modale
+    const modal = await this.modalController.getTop();
+    if (modal) {
+      // Se c'è un modale aperto, chiudi il modale
+      modal.dismiss();
+    } else {
+      // Altrimenti, esegui il comportamento predefinito del back button
+      this.navController.back();
+    }
+  }
 }
