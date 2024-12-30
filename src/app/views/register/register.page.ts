166a60d70863d1c1d42619c40b97fc7e34da8fc9
user265838 <webmastercasale@gmail.com>
2024-12-30 22:00:18 +0100

    IOS BUG FIX

--- src/app/views/register/register.page.ts
+++ src/app/views/register/register.page.ts
@@ -39,6 +39,8 @@ export class RegisterPage {
     const cognome = !registerData.cognome ? '' : registerData.cognome
     const password = registerData.password;
     const email = registerData.email ;
+    const gender = !registerData.gender ? 'D' : registerData.gender;
+
     let userProfile:Partial<UserProfile> ={
       displayName:displayName,
       email: email,
