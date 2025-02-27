02ae1cd1d3905f8dffd71d98143469ab8bc38e30
webmastercasale@gmail.com <webmastercasale@gmail.com>
2025-02-27 13:21:07 +0100

    shared 2.1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,7 +6,7 @@ const config: CapacitorConfig = {
   webDir: 'www',
   bundledWebRuntime: false,
   server: {
-    androidScheme: 'https',
+    androidScheme: 'comemivesto',
     allowNavigation: ["*"]
   },
   
