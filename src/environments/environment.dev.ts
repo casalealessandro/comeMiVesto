2f90c6ab42d3bbd5611638bb3b72107b895db755
Alessandro Casale <81802080+casalealessandro@users.noreply.github.com>
2026-09-01 16:59:46 +0200

    fix: restore apiDev frontend environment

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    production:false,
+    BASE_API_URL: 'https://us-central1-comemivesto-5e5f9.cloudfunctions.net/apiDev',
+    useEmulators: false,
+    firebase: {
+        apiKey: "AIzaSyBNWYDp0fPgLU0npKMC0RoS6C14fHp2mpk",
+        authDomain: "comemivesto-5e5f9.firebaseapp.com",
+        projectId: "comemivesto-5e5f9",
+        storageBucket: "comemivesto-5e5f9.appspot.com",
+        messagingSenderId: "124990581718",
+        appId: "1:124990581718:web:51c8cbf699123bd0e8d277",
+        measurementId: "G-8D9XBPY0Q7"
+      }
+
+
+};
