e7368f46eb42a2ec85951a89bab1687d6cb92590
Alessandro Casale <81802080+casalealessandro@users.noreply.github.com>
2026-09-15 15:42:12 +0200

    test: isolate Header component dependencies

--- src/app/layout/header/header.component.spec.ts
+++ src/app/layout/header/header.component.spec.ts
@@ -1,6 +1,11 @@
+import { signal } from '@angular/core';
+import { provideRouter } from '@angular/router';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { EMPTY } from 'rxjs';
+import { MenuController, ModalController, NavController } from '@ionic/angular';
 
+import { SharedDataService } from 'src/app/service/shared-data.service';
+import { UserService } from 'src/app/service/user.service';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
@@ -9,9 +14,46 @@ describe('HeaderComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+      declarations: [HeaderComponent],
+      providers: [
+        provideRouter([]),
+        {
+          provide: UserService,
+          useValue: {
+            gUserProfile: () => signal({ uid: 'test-user' } as any)
+          }
+        },
+        {
+          provide: SharedDataService,
+          useValue: {
+            data: signal<any[]>([]),
+            staredData$: EMPTY
+          }
+        },
+        {
+          provide: ModalController,
+          useValue: { getTop: jasmine.createSpy('getTop').and.resolveTo(null) }
+        },
+        {
+          provide: NavController,
+          useValue: { back: jasmine.createSpy('back') }
+        },
+        {
+          provide: MenuController,
+          useValue: {
+            close: jasmine.createSpy('close'),
+            open: jasmine.createSpy('open'),
+            isOpen: jasmine.createSpy('isOpen').and.resolveTo(false)
+          }
+        }
+      ]
+    });
+
+    TestBed.overrideComponent(HeaderComponent, {
+      set: { template: '' }
+    });
+
+    TestBed.compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
