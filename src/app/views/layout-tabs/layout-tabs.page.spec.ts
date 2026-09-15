1c36be76a852d9bc71a6db9813a1d852cce8f64a
Alessandro Casale <81802080+casalealessandro@users.noreply.github.com>
2026-09-15 15:48:42 +0200

    test: modernize LayoutTabs smoke test

--- src/app/views/layout-tabs/layout-tabs.page.spec.ts
+++ src/app/views/layout-tabs/layout-tabs.page.spec.ts
@@ -1,15 +1,40 @@
+import { provideRouter } from '@angular/router';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NavController } from '@ionic/angular';
+
+import { SharedDataService } from 'src/app/service/shared-data.service';
 import { LayoutTabsPage } from './layout-tabs.page';
 
 describe('LayoutTabsPage', () => {
   let component: LayoutTabsPage;
   let fixture: ComponentFixture<LayoutTabsPage>;
 
-  beforeEach(async(() => {
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      declarations: [LayoutTabsPage],
+      providers: [
+        provideRouter([]),
+        {
+          provide: SharedDataService,
+          useValue: { setData: jasmine.createSpy('setData') }
+        },
+        {
+          provide: NavController,
+          useValue: { back: jasmine.createSpy('back') }
+        }
+      ]
+    });
+
+    TestBed.overrideComponent(LayoutTabsPage, {
+      set: { template: '' }
+    });
+
+    await TestBed.compileComponents();
+
     fixture = TestBed.createComponent(LayoutTabsPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
