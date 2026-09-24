02f6ad115e58fc9378c2ea2ca7cdfe94bf1ca07e
Alessandro Casale <81802080+casalealessandro@users.noreply.github.com>
2026-09-24 16:19:14 +0200

    fix(auth): register legacy Google Sign-In plugin

--- android/app/src/main/java/com/acasale/comemivesto/MainActivity.java
+++ android/app/src/main/java/com/acasale/comemivesto/MainActivity.java
@@ -1,5 +1,13 @@
 package com.acasale.comemivesto;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(LegacyGoogleSignInPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
